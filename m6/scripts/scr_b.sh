e6ebcf2a28a6d77f2825cb35e32208c222653b11
MT <timchenko.mi@hotmail.com>
2022-05-10 16:57:21 +0300

    scr_b

--- m6/scripts/scr_b.sh
+++ m6/scripts/scr_b.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 echo Most frequncy ip:
+awk '{print $1}' /home/u/GitHub/DevOps_online_Kharkiv_2022_1/m6/apache_logs.txt | sort | uniq -c | sort -nr | head -n 15
 echo _______________________
-awk '{ print $1}' /home/u/GitHub/DevOps_online_Kharkiv_2022_1/m6/apache_logs.txt | sort | uniq -c | sort -nr | head -n 15
-
 
 echo Most frequncy page:
+awk '{print $7}' /home/u/GitHub/DevOps_online_Kharkiv_2022_1/m6/apache_logs.txt | sort | uniq -c | sort -nr | head -n 20
 echo _______________________
-awk '{ print $7}' /home/u/GitHub/DevOps_online_Kharkiv_2022_1/m6/apache_logs.txt | sort | uniq -c | sort -nr | head -n 20
+
+echo Most frequncy time:
+awk '{print substr($4, 2, 17)}' /home/u/GitHub/DevOps_online_Kharkiv_2022_1/m6/apache_logs.txt | sort | uniq -c | sort -nr | head -n 1
 echo _______________________
-awk '{ print $4}' /home/u/GitHub/DevOps_online_Kharkiv_2022_1/m6/apache_logs.txt | sort | uniq -c | sort -nr | head -n 3
